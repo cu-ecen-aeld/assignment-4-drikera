5338e1f5d414db70b654ccc79fedde2cccd6a6db
adriker <adriker@UbuntuAESD.myguest.virtualbox.org>
2025-08-13 18:52:13 -0700

    Updated commit for assignments-3..

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_VERSION = 77ffd1744679fe389d72ac07ee62ef50b383217e
+AESD_ASSIGNMENTS_VERSION = b6578b9356d6926c075a54d5ea105f6d0bec3ed8
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
